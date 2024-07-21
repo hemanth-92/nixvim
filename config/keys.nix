0c7c042228c6b039a46269bf072de71ba255bff8
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-21 08:49:00 +0530

    updated keys

--- config/keys.nix
+++ config/keys.nix
@@ -11,6 +11,12 @@ in
   keymaps = [
     # General maps
 
+    # fix page up and page down so the cursor doesn't move
+    (bind "n" "<PageUp>" "<C-U>")
+    (bind "n" "<PageDown>" "<C-D>")
+    (bind "i" "<PageUp>" "<C-O><C-U>")
+    (bind "i" "<PageDown>" "<C-O><C-D>")
+
     # ctrl-s to save
     (bind "n" "<C-S>" ":w<CR>")
     (bind "i" "<C-S>" "<C-O>:up<CR>")
@@ -22,11 +28,12 @@ in
     # Y copies to end of line
     (bind "n" "Y" "y$")
 
-    # Do stuff whilst keeping the cursor in the center.
-    (bind "n" "<C-d>" "<C-d>zz")
-    (bind "n" "<C-u>" "<C-u>zz")
-    (bind "n" "n" "nzzzv")
-    (bind "n" "N" "Nzzzv")
+    # keep cursor in place when joining lines
+    (bind "n" "J" "mzJ`z")
+
+    # visual shifting (does not exit visual mode)
+    (bind "v" "<" "<gv")
+    (bind "v" ">" ">gv")
 
     # copy and paste to clipboard
     (bind "v" "<C-C>" ''"+y'')
@@ -41,18 +48,57 @@ in
     (bindopts "v" "p" "p`]" { silent = true; })
     (bindopts "n" "p" "p`]" { silent = true; })
 
+    # reselect text
+    (bind "v" "gV" "`[v`]")
+
+    # disable F1 key
+    (bind "n" "<F1>" "<Esc>")
+    (bind "i" "<F1>" "<Esc>")
+    (bind "v" "<F1>" "<Esc>")
+
+    # TODO: disable manual key k?
+    # jk or kj to escape insert mode
     (bind "i" "jk" "<Esc>")
     (bind "i" "kj" "<Esc>")
 
-    # vim fugitive
-    (bind "n" "<leader>gs" ":G<CR>")
+    # center display after searches
+    (bind "n" "n" "nzzzv")
+    (bind "n" "N" "Nzzzv")
+    (bind "n" "*" "*zzzv")
+    (bind "n" "#" "#zzzv")
+    (bind "n" "g*" "g*zzzv")
+    (bind "n" "g#" "g#zzzv")
 
-    # better quickfix navigation
-    (bind "n" "<C-J>" ":cnext<CR>")
-    (bind "n" "<C-K>" ":cprevious<CR>")
+    # only jumps of more than 5 lines are added to the jumplist
+    (bindopts "n" "k" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'k'" { expr = true; })
+    (bindopts "n" "j" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'j'" { expr = true; })
+
+    # vv enter visual block mode
+    (bind "n" "vv" "<C-V>")
+
+    # ; is an alias for :
+    (bind "n" ";" ":")
+
+    # better command line editing
+    (bind "c" "<C-A>" "<Home>")
+    (bind "c" "<C-E>" "<End>")
 
     # easier buffer navigation
     (bind "n" "<Tab>" ":bnext<CR>")
     (bind "n" "<S-Tab>" ":bprevious<CR>")
+
+    # swap functionality of gj and gk
+    (bind "n" "j" "gj")
+    (bind "n" "k" "gk")
+    (bind "n" "gj" "j")
+    (bind "n" "gk" "k")
+
+    # TODO: incsearch?
+    # better quickfix navigation
+    (bind "n" "<C-J>" ":cnext<CR>")
+    (bind "n" "<C-K>" ":cprevious<CR>")
+
+    # vim fugitive
+    (bind "n" "<leader>gs" ":G<CR>")
   ];
 }
