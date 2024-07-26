cbd7644b1fafb4135e2d7805362559f1fcb613f1
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-26 07:41:39 +0530

    trace errors

--- config/plug/lsp/none-ls.nix
+++ config/plug/lsp/none-ls.nix
@@ -1,47 +1,54 @@
 {
-    plugins.none-ls = {
-      enable = true;
-      enableLspFormat = true;
-      settings = { update_in_insert = false; };
-      sources = {
-        code_actions = {
-          gitsigns.enable = true;
-          statix.enable = true;
+  plugins.none-ls = {
+    enable = true;
+    enableLspFormat = true;
+    settings = {
+      update_in_insert = false;
+    };
+    sources = {
+      code_actions = {
+        gitsigns.enable = true;
+        statix.enable = true;
+      };
+      diagnostics = {
+        statix.enable = true;
+        yamllint.enable = true;
+      };
+      formatting = {
+        black = {
+          enable = true;
+          settings = ''
+            {
+              extra_args = { "--fast" },
+            }
+          '';
         };
-        diagnostics = {
-          statix.enable = true;
-          yamllint.enable = true;
+        prettier = {
+          enable = true;
+          disableTsServerFormatter = true;
+          settings = ''
+            {
+              extra_args = { "--no-semi", "--single-quote" },
+            }
+          '';
         };
-        formatting = {
-          black = {
-            enable = true;
-            withArgs = ''
-              {
-                extra_args = { "--fast" },
-              }
-            '';
-          };
-          prettier = {
-            enable = true;
-            disableTsServerFormatter = true;
-            withArgs = ''
-              {
-                extra_args = { "--no-semi", "--single-quote" },
-              }
-            '';
-          };
-          stylua.enable = true;
-          yamlfmt.enable = true;
-        };
+        stylua.enable = true;
+        yamlfmt.enable = true;
       };
     };
-    keymaps = [{
-      mode = [ "n" "v" ];
+  };
+  keymaps = [
+    {
+      mode = [
+        "n"
+        "v"
+      ];
       key = "<leader>cf";
       action = "<cmd>lua vim.lsp.buf.format()<cr>";
       options = {
         silent = true;
         desc = "Format";
       };
-    }];
+    }
+  ];
 }
