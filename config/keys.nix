0bda7ae658bf6322b0c200cc4112f29cc670919e
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-07-27 12:47:47 +0530

    fix:keys

--- config/keys.nix
+++ config/keys.nix
@@ -1,8 +1,8 @@
 let
-  bind = mode: key: action: { inherit mode key action; };
-  bindopts =
+  mkKeymap = mode: key: action: { inherit mode key action; };
+  mkKeymapWithOpts =
     mode: key: action: opts:
-    (bind mode key action) // { options = opts; };
+    (mkKeymap mode key action) // { options = opts; };
 
 in
 {
@@ -12,93 +12,95 @@ in
     # General maps
 
     # fix page up and page down so the cursor doesn't move
-    (bind "n" "<PageUp>" "<C-U>")
-    (bind "n" "<PageDown>" "<C-D>")
-    (bind "i" "<PageUp>" "<C-O><C-U>")
-    (bind "i" "<PageDown>" "<C-O><C-D>")
+    (mkKeymap "n" "<PageUp>" "<C-U>")
+    (mkKeymap "n" "<PageDown>" "<C-D>")
+    (mkKeymap "i" "<PageUp>" "<C-O><C-U>")
+    (mkKeymap "i" "<PageDown>" "<C-O><C-D>")
 
     # ctrl-s to save
-    (bind "n" "<C-S>" ":w<CR>")
-    (bind "i" "<C-S>" "<C-O>:up<CR>")
-    (bind "v" "<C-S>" "<C-C>:up<CR>")
+    (mkKeymap "n" "<C-S>" ":w<CR>")
+    (mkKeymap "i" "<C-S>" "<C-O>:up<CR>")
+    (mkKeymap "v" "<C-S>" "<C-C>:up<CR>")
 
     # L to go to the end of the line
-    (bind "n" "L" "$")
+    (mkKeymap "n" "L" "$")
 
     # Y copies to end of line
-    (bind "n" "Y" "y$")
+    (mkKeymap "n" "Y" "y$")
 
     # keep cursor in place when joining lines
-    (bind "n" "J" "mzJ`z")
+    (mkKeymap "n" "J" "mzJ`z")
 
     # visual shifting (does not exit visual mode)
-    (bind "v" "<" "<gv")
-    (bind "v" ">" ">gv")
+    (mkKeymap "v" "<" "<gv")
+    (mkKeymap "v" ">" ">gv")
 
     # copy and paste to clipboard
-    (bind "v" "<C-C>" ''"+y'')
-    (bind "n" "<C-V>" ''"+P'')
-    (bind "i" "<C-V>" ''<C-O>"+P'')
+    (mkKeymap "v" "<C-C>" ''"+y'')
+    (mkKeymap "n" "<C-V>" ''"+P'')
+    (mkKeymap "i" "<C-V>" ''<C-O>"+P'')
 
     # replace highlighted text when pasting
-    (bind "v" "<C-V>" ''"+P'')
+    (mkKeymap "v" "<C-V>" ''"+P'')
 
     # automatically jump to end of text pasted
-    (bindopts "v" "y" "y`]" { silent = true; })
-    (bindopts "v" "p" "p`]" { silent = true; })
-    (bindopts "n" "p" "p`]" { silent = true; })
+    (mkKeymapWithOpts "v" "y" "y`]" { silent = true; })
+    (mkKeymapWithOpts "v" "p" "p`]" { silent = true; })
+    (mkKeymapWithOpts "n" "p" "p`]" { silent = true; })
 
     # reselect text
-    (bind "v" "gV" "`[v`]")
+    (mkKeymap "v" "gV" "`[v`]")
 
     # disable F1 key
-    (bind "n" "<F1>" "<Esc>")
-    (bind "i" "<F1>" "<Esc>")
-    (bind "v" "<F1>" "<Esc>")
+    (mkKeymap "n" "<F1>" "<Esc>")
+    (mkKeymap "i" "<F1>" "<Esc>")
+    (mkKeymap "v" "<F1>" "<Esc>")
 
     # TODO: disable manual key k?
+
     # jk or kj to escape insert mode
-    (bind "i" "jk" "<Esc>")
-    (bind "i" "kj" "<Esc>")
+    (mkKeymap "i" "jk" "<Esc>")
+    (mkKeymap "i" "kj" "<Esc>")
 
     # center display after searches
-    (bind "n" "n" "nzzzv")
-    (bind "n" "N" "Nzzzv")
-    (bind "n" "*" "*zzzv")
-    (bind "n" "#" "#zzzv")
-    (bind "n" "g*" "g*zzzv")
-    (bind "n" "g#" "g#zzzv")
+    (mkKeymap "n" "n" "nzzzv")
+    (mkKeymap "n" "N" "Nzzzv")
+    (mkKeymap "n" "*" "*zzzv")
+    (mkKeymap "n" "#" "#zzzv")
+    (mkKeymap "n" "g*" "g*zzzv")
+    (mkKeymap "n" "g#" "g#zzzv")
 
     # only jumps of more than 5 lines are added to the jumplist
-    (bindopts "n" "k" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'k'" { expr = true; })
-    (bindopts "n" "j" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'j'" { expr = true; })
+    (mkKeymapWithOpts "n" "k" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'k'" { expr = true; })
+    (mkKeymapWithOpts "n" "j" "(v:count > 5 ? \"m'\" . v:count : \"\") . 'j'" { expr = true; })
 
     # vv enter visual block mode
-    (bind "n" "vv" "<C-V>")
+    (mkKeymap "n" "vv" "<C-V>")
 
     # ; is an alias for :
-    (bind "n" ";" ":")
+    (mkKeymap "n" ";" ":")
 
     # better command line editing
-    (bind "c" "<C-A>" "<Home>")
-    (bind "c" "<C-E>" "<End>")
+    (mkKeymap "c" "<C-A>" "<Home>")
+    (mkKeymap "c" "<C-E>" "<End>")
 
     # easier buffer navigation
-    (bind "n" "<Tab>" ":bnext<CR>")
-    (bind "n" "<S-Tab>" ":bprevious<CR>")
+    (mkKeymap "n" "<Tab>" ":bnext<CR>")
+    (mkKeymap "n" "<S-Tab>" ":bprevious<CR>")
 
     # swap functionality of gj and gk
-    (bind "n" "j" "gj")
-    (bind "n" "k" "gk")
-    (bind "n" "gj" "j")
-    (bind "n" "gk" "k")
+    (mkKeymap "n" "j" "gj")
+    (mkKeymap "n" "k" "gk")
+    (mkKeymap "n" "gj" "j")
+    (mkKeymap "n" "gk" "k")
 
     # TODO: incsearch?
+
     # better quickfix navigation
-    (bind "n" "<C-J>" ":cnext<CR>")
-    (bind "n" "<C-K>" ":cprevious<CR>")
+    (mkKeymap "n" "<C-J>" ":cnext<CR>")
+    (mkKeymap "n" "<C-K>" ":cprevious<CR>")
 
     # vim fugitive
-    (bind "n" "<leader>gs" ":G<CR>")
+    (mkKeymap "n" "<leader>gs" ":G<CR>")
   ];
 }
