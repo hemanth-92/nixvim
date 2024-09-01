35a51fa9e09363a95954e622dceb106f42dd14b9
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-09-01 12:18:57 +0530

    fix:trace errors

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -1,59 +1,62 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      python = [ "black" ];
-      nix = [ "nixfmt" ];
-      markdown = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      rust = [ "rustfmt" ];
-      html = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      css = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      javascript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      javascriptreact = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      typescript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      typescriptreact = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      java = [ "google-java-format" ];
-      clangd = [ " clang_format" ];
+    settings = {
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notify_on_error = true;
+      formatters_by_ft = {
+        python = [ "black" ];
+        nix = [ "nixfmt" ];
+        markdown = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        rust = [ "rustfmt" ];
+        html = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        css = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        java = [ "google-java-format" ];
+        clangd = [ " clang_format" ];
+      };
+
     };
   };
 }
