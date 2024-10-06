f755d8316f5393c82a3fa86ab6b4264153f798c4
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-10-06 19:27:42 +0530

    remove emojis

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -27,7 +27,6 @@
         };
         sources = [
           { name = "nvim_lsp"; }
-          { name = "emoji"; }
           {
             name = "buffer"; # text within current buffer
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
