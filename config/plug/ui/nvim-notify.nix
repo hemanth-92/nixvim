9fd3127c430eee1e31bac071144b5ba91ad046b1
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2025-03-02 13:20:01 +0530

    update and fix evaluation warnings

--- config/plug/ui/nvim-notify.nix
+++ config/plug/ui/nvim-notify.nix
@@ -1,11 +1,13 @@
 {
   plugins.notify = {
     enable = true;
-    backgroundColour = "#000000";
-    fps = 60;
-    render = "default";
-    timeout = 1000;
-    topDown = true;
+    settings = {
+      backgroundColour = "#000000";
+      fps = 60;
+      render = "default";
+      timeout = 1000;
+      topDown = true;
+    };
   };
   keymaps = [
     {
